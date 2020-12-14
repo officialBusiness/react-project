eaacbf5d2f3677778a38b7e47859734d6b93a032
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2020-12-14 22:57:49 +0800

    开始树组件拖拽和线条的添加

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -2,52 +2,87 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Triangle from './Triangle.js'
 import './Tree.scss'
-import { getNoneSize } from '../utils.js'
+import { getSize, Animation } from '../utils.js'
 
 class TreeLi extends React.Component{
-  constructor(props) {
-    super(props);
-		if ( this.props.data.show === void 0 ) {
-			this.props.data.show = false
+	constructor(props) {
+		super(props);
+		let data = props.data
+		if ( data.show === undefined ) {
+			data.show = false
 		}
-    this.state = {
-    	show: this.props.data.show,
-    }
-		console.log( this.state.show )
-  }
+		this.state = {
+			ulShow: data.show,
+		}
+		this.treeUlRef = React.createRef()
+		this.hasChildren = data.children && Array.isArray( data.children ) && data.children.length > 0
+	}
+	UNSAFE_componentWillMount(){
+		this.treeUlDom = ReactDOM.findDOMNode(this.treeUlRef.current)
+	}
 	render(){
 		let { data = {}, level, icon } = this.props ,
-				hasChildren = data.children && Array.isArray( data.children ) && data.children.length > 0 ,
-				iconRef = React.createRef() ,
-				treeUlRef = React.createRef()
+				iconRef = React.createRef()
 		return (
 			<li className={'treeLi'}>
 				<div className={'rowContainer'}>
 					<div className={'treeLiIconContainer columnContainer'}
 						style={{
-							visibility: hasChildren ? 'visible' : 'hidden'
-						}} 
+							visibility: this.hasChildren ? 'visible' : 'hidden'
+						}}
 						onClick={(e)=>{
-							let treeUlDom = ReactDOM.findDOMNode(treeUlRef.current)
-							console.log( getNoneSize(treeUlDom) )
-							this.setState({
-								show: !this.state.show
+							if ( !this.hasChildren ) {
+								return
+							}
+							let treeUlDom = ReactDOM.findDOMNode(this.treeUlRef.current),
+									height = getSize( treeUlDom ).height,
+									ulShow = data.show
+							Animation({
+								operation: treeUlDom.style,
+								from: {
+									height: ulShow ? height : 0,
+									opacity: ulShow ? 1 : 0 ,
+								},
+								duration: 300,
+								to: {
+									height: ulShow ? 0 : height ,
+									opacity: ulShow ? 0 : 1 ,
+								},
+								onStart: (operation, from, to)=>{
+									if ( ulShow ) {
+										treeUlDom.style.height = height + 'px'
+									}else{
+										treeUlDom.style.display = 'block'
+									}
+								},
+								onUpdate: (operation, from, to, now)=>{
+									operation.height = now.height + 'px'
+									operation.opacity = now.opacity
+								},
+								onComplete: (operation, from, to)=>{
+									data.show = !ulShow
+									treeUlDom.style.height = 'auto'
+									if ( ulShow ) {	//已经完全隐藏
+										treeUlDom.style.display = 'none'
+									}else{	//ul已经完全展开
+									}
+								}
 							})
-							if ( this.state.show ) {
+							if ( ulShow ) {
 								iconRef.current.style.transform = `rotate(0deg)`
 							}else{
 								iconRef.current.style.transform = `rotate(90deg)`
 							}
 						}}>
 							<div className={'treeLiIcon rowContainer'} ref={iconRef} style={{
-								transform: this.state.show ? `rotate(90deg)` : `rotate(0deg)`
+								transform: this.state.ulShow ? `rotate(90deg)` : `rotate(0deg)`
 							}}>{icon}</div>
 						</div>
 					<div className={'treeLiTitle'}>{data.title}</div>
 				</div>
 				{
-					hasChildren ?
-					<TreeUl ref={treeUlRef} data={data.children} level={level + 1} icon={icon} show={this.state.show}/> : null
+					this.hasChildren ?
+					<TreeUl ref={this.treeUlRef} data={data.children} level={level + 1} icon={icon} show={data.show}/> : null
 				}
 			</li>
 		)
@@ -55,7 +90,7 @@ class TreeLi extends React.Component{
 }
 class TreeUl extends React.Component{
 	render(){
-		let { level, show = false , data=[], icon } = this.props
+		let { level, show, data=[], icon } = this.props
 		return (
 			<ul
 				className={'treeUl'}
@@ -78,6 +113,7 @@ function Tree({
 	data = [],
 	icon = <Triangle direction={'right'} width={3} height={6} />,
 }){
+	// console.log( data )
 	return (
 		<div className={'Tree'}>
 			<TreeUl
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,94 @@
 // 获取display为none的dom的宽高位置
-export function getNoneSize(dom){
+export function getSize(dom){
+	let backup = {
+		position: dom.style.position,
+		visibility: dom.style.visibility,
+		display: dom.style.display,
+	}
 	dom.style.position = 'absolute'
 	dom.style.visibility = 'hidden'
-	dom.style.display = 'block'
+	if ( dom.style.display === 'none' ) {
+		dom.style.display = 'block'
+	}
 	let attr = {
 		height: dom.offsetHeight,
 		width: dom.offsetWidth,
 		offsetLeft: dom.offsetLeft,
 		offsetTop: dom.offsetTop
 	}
-	dom.style.position = 'initial'
-	dom.style.visibility = 'initial'
-	dom.style.display = 'none'
+	dom.style.position = backup.position
+	dom.style.visibility = backup.visibility
+	dom.style.display = backup.display
 	return attr
 }
-// 简单的动画变化
-export function Animation(){
-
+// 获取当前时间
+export const now$1 = window.performance.now.bind(window.performance)
+// 深拷贝一个对象
+export function clone(obj){
+	var ret = {}
+	for(var key in obj){
+		if ( typeof obj[key] === 'object' ) {
+			ret[key] = clone(obj[key])
+		}else{
+			ret[key] = obj[key]
+		}
+	}
+	return ret
 }
+// 简单的动画变化函数
+export function Animation({
+	operation = {},
+	from = {},
+	to = {},
+	duration = 1000,
+	onStart = ()=>{},
+	onUpdate = ()=>{},
+	onComplete = ()=>{},
+}){
+	if ( operation.isAnnmating ) {
+		return
+	}
+	onStart(operation, from, to)
+	operation.isAnnmating = true
+	var startTime = now$1(),
+			endTime = startTime + duration,
+			tempObj = {}
+
+	initObject(tempObj, to)
+	var animationId = requestAnimationFrame(doAnimation)
+	function doAnimation(time){
+		var percentage = (time <= startTime ? 0 : time >= endTime ? 1 : (time - startTime) / duration)
+		updateProperties( from, to, tempObj, percentage )
+		onUpdate( operation, from, to, tempObj )
+		if ( percentage === 1 ) {
+			cancelAnimationFrame(animationId)
+			onComplete(operation, from, to)
+			operation.isAnnmating = false
+		}else{
+			requestAnimationFrame(doAnimation)
+		}
+	}
+}
+function initObject(object, to){
+	for( var key in to ){
+		if ( typeof to[key] === 'object' ) {
+			object[key] = {}
+			initObject( object[key], to[key] )
+		}else{
+			object[key] = to[key]
+		}
+	}
+}
+function updateProperties(from, to, object, percentage){
+	for( var key in to ){
+		if ( typeof to[key] === 'object' ) {
+			updateProperties( from[key], to[key], object[key], percentage )
+		}else if ( typeof to[key] === 'number' ) {
+			object[key] = (to[key] - from[key]) * percentage + from[key]
+		}
+	}
+}
+
 // 转化three的三维位置到浏览器窗口的位置
 export function getWindowPositionFromThree(threePosition){
 	
