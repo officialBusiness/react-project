b965c08ed6e264ecced59e62c8d644d3ea6e07d6
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2021-02-03 22:05:49 +0800

    开始研究ks代码解析可视化

---
+++ src/Test/JsCodeToSvgFlowchart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import js2flowchart from './Js2flowchart/Js2flowchart.js'
+
+export default class JsCodeToSvgFlowchart extends React.Component{
+	render(){
+		return (
+			<div className={'JsCodeToSvgFlowchart'} >
+			</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -7,15 +7,17 @@ import ThreeOperation from './ThreeOperation/ThreeOperation.js'
 import ComponentsTest from './Test/ComponentsTest.js'
 // import DvaTest from './Test/DvaTest.js'
 import RichTextBoxTest from './Test/RichTextBoxTest.js'
+import JsCodeToSvgFlowchart from './Test/JsCodeToSvgFlowchart.js'
 import notFound from './404.js'
 
 ReactDOM.render(
 	<Router>
 		<Switch>
 			<Route path="/ThreeOperation" component={ThreeOperation}/>
-			<Route path="/ComponentsTest" component={ComponentsTest}/>
 			<Route path="/404" component={notFound}/>
-			<Route path="/RichTextBoxTest" component={RichTextBoxTest}/>
+			<Route path="/Test/ComponentsTest" component={ComponentsTest}/>
+			<Route path="/Test/RichTextBoxTest" component={RichTextBoxTest}/>
+			<Route path="/Test/JsCodeToSvgFlowchart" component={JsCodeToSvgFlowchart}/>
 			<Route path="/" component={App}/>
 		</Switch>
 	</Router>,
