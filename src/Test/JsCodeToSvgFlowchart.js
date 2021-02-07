be2fc6d023591bde8c3a9f3d742c75693974efe5
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2021-02-07 21:48:36 +0800

    完成js-code-to-svg-flowchart代码迁移

--- src/Test/JsCodeToSvgFlowchart.js
+++ src/Test/JsCodeToSvgFlowchart.js
@@ -1,10 +1,44 @@
 import React from 'react'
-// import js2flowchart from './Js2flowchart/Js2flowchart.js'
+import { convertCodeToSvg } from './Js2flowchart/Js2flowchart.js';
+const code = `
+    class A { b = 1; }
+
+    function indexSearch(list, element) {
+        let currentIndex,
+            currentElement,
+            minIndex = 0,
+            maxIndex = list.length - 1;
+
+        while (minIndex <= maxIndex) {
+            currentIndex = Math.floor((maxIndex + maxIndex) / 2);
+            currentElement = list[currentIndex];
+
+            if (currentElement === element) {
+                return currentIndex;
+            }
+
+            if (currentElement < element) {
+                minIndex = currentIndex + 1;
+            }
+
+            if (currentElement > element) {
+                maxIndex = currentIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+`;
 
 export default class JsCodeToSvgFlowchart extends React.Component{
+	componentDidMount(){
+		this.flowchartDom.innerHTML = convertCodeToSvg(code)
+	}
 	render(){
 		return (
-			<div className={'JsCodeToSvgFlowchart'} >
+			<div className={'JsCodeToSvgFlowchart'} ref={(dom)=>{
+				this.flowchartDom = dom
+			}}>
 			</div>
 		)
 	}
